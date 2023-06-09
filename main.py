90dabbde4598bcbb6d5ba677c97f69f290aff2f8
Eduard Karol <korol94.94@gmail.com>
2023-06-09 18:25:44 +0300

    after race button now refresh to start button, add leader board from finished players hashtable

--- main.py
+++ main.py
@@ -19,31 +19,43 @@ async def start_handler(message: types.Message):
 
 players = {}
 race_done_mark = False
+finished_players = {}
 
 @dp.message_handler(content_types=types.ContentTypes.LOCATION)
 async def handle_location(message: types.Message):
-    global race_done_mark
     username = message.from_user.username
     full_name = message.from_user.full_name
     start_latitude = message.location.latitude
     start_longitude = message.location.longitude
-    if username in players and race_done_mark:
-        finish_latitude = message.location.latitude
-        finish_longitude = message.location.longitude
-        distance = round(haversine(players[username], (finish_latitude, finish_longitude)), 3) * 1000
-        await message.answer(f'{full_name}, вы прошли {distance} метра')
-        del players[username]
-    else:
-        players[username] = [start_latitude, start_longitude]
-        print(players)
-        secs = 5
-        while secs != 0:
-            time.sleep(1)
-            await message.answer(f'Осталось {secs} секунд до конца гонки!')
-            secs -= 1
-        race_done_mark = True
-        await message.answer(f'Гонка завершена')
-        await message.answer(f'Жми на кнопку!', reply_markup=menu.mainMenuFinish)
+    async def calculate_distance():
+        global race_done_mark
+        if username in players and race_done_mark:
+            finish_latitude = message.location.latitude
+            finish_longitude = message.location.longitude
+            distance = round(haversine(players[username], (finish_latitude, finish_longitude)), 3) * 1000
+            await message.answer(f'{full_name}, вы прошли {distance} метра', reply_markup=menu.mainMenu)
+            await message.answer(f'Таблица лидеров:')
+            finished_players[username]=distance
+            for idx, pl in enumerate(sorted(finished_players), start=1):
+                await message.answer(f'{idx}. {pl} - {finished_players[pl]} метра')
+            print(finished_players)
+            del players[username]
+
+        else:
+            players[username] = [start_latitude, start_longitude]
+            print(players)
+            secs = 2
+            timeout = secs / 2
+            while secs != 0:
+                time.sleep(timeout)
+                await message.answer(f'Осталось {secs} секунд до конца гонки!')
+                secs -= int(timeout)
+            race_done_mark = True
+            await message.answer(f'Гонка завершена')
+            await message.answer(f'Жми на кнопку!', reply_markup=menu.mainMenuFinish)
+    if 1:
+        await calculate_distance()
+
 
 
 if __name__ == '__main__':

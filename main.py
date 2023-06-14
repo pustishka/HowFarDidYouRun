14e5dc0e524e2b1925d4e1d1924ee8718c2edf55
Eduard Karol <korol94.94@gmail.com>
2023-06-14 12:31:35 +0300

    probably adding threading with async, starting adding comments

--- main.py
+++ main.py
@@ -2,76 +2,91 @@ import time
 import menu
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardRemove
-
+import threading
 from haversine import haversine
 
-TOKEN = '5832582610:AAHnBKGpVEeIT__6eRq9-6u831j8i0BzaIc'
+TOKEN = '5832582610:AAHnBKGpVEeIT__6eRq9-6u831j8i0BzaIc'  # Token from @BotFather
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 
-players = {}
-race_done_mark = False
-finished_players = {'ed': [384713574, 150], 'ed1': [384713574, 3500]}
+players = {}  # hashtable for accumulating members of bot
+race_done_mark = False  # Mark using for access to second step (generate leader_board)
+finished_players = {'ed': [384713574, 150], 'ed1': [384713574,
+                                                    3500]}  # hashtable for players whos done first step (set point of first location)
 
 
+# main start handler for command in bot /start
 @dp.message_handler(commands=['start'])
 async def start_handler(message: types.Message):
-    user_full_name = message.from_user.full_name
-    await message.answer(f'Привет, {user_full_name}', reply_markup=menu.mainMenu)
+    user_full_name = message.from_user.full_name  # get full_name of user from message
+    await message.answer(f'Привет, {user_full_name}', reply_markup=menu.mainMenu)  # bot sent 'Hello' to user
 
 
-@dp.message_handler(content_types=types.ContentTypes.LOCATION)
-async def handle_location(message: types.Message):
-    username = message.from_user.username
-    full_name = message.from_user.full_name
-    start_latitude = message.location.latitude
-    start_longitude = message.location.longitude
+# potential func for threading
+def main_func():
+    # main handler for all process in bot
+    @dp.message_handler(content_types=types.ContentTypes.LOCATION)
+    async def handle_location(message: types.Message):
+        username = message.from_user.username
+        full_name = message.from_user.full_name
+        start_latitude = message.location.latitude  # get latitude and longitude from message location
+        start_longitude = message.location.longitude
 
-    async def calculate_distance():
-        global race_done_mark
-        if username in players and race_done_mark:
-            finish_latitude = message.location.latitude
-            finish_longitude = message.location.longitude
-            distance = round(haversine(players[username], (finish_latitude, finish_longitude)), 3) * 1000
-            await message.answer(f'{full_name}, вы прошли {distance} метра', reply_markup=menu.mainMenu)
+        # func for calculation difference of lat and long and convert to meters
+        async def calculate_distance():
+            global race_done_mark
+            if username in players and race_done_mark:  # condition for access to second step (calculate distance from second long and lat)
+                finish_latitude = message.location.latitude
+                finish_longitude = message.location.longitude
+                distance = round(haversine(players[username], (finish_latitude, finish_longitude)),
+                                 3) * 1000  # haversine can calculate distance
+                await message.answer(f'{full_name}, вы прошли {distance} метра',
+                                     reply_markup=menu.mainMenu)  # sent to user passed distance
 
-            async def generate_leaderboard():
-                global finished_players
-                ids = message.from_user.id
-                finished_players[username] = [ids, distance]
-                del players[username]
-                leader_board = ''
-                if 3 <= len(finished_players) <= 5:
-                    await message.answer(f'Таблица лидеров:')
-                    for idx, pl in enumerate(dict(sorted(finished_players.items(), key=lambda x: x[1], reverse=True)),
-                                             start=1):
-                        leader_board += f'{idx}. {pl} - {finished_players[pl][1]} метра\n'
-                    for ids in finished_players.values():
-                        await bot.send_message(chat_id=ids[0], text=leader_board)
-                    finished_players = {}
+                # func for generate leaderboard and sent to every user in finished players
+                async def generate_leaderboard():
+                    global finished_players
+                    ids = message.from_user.id  # get id of user from message
+                    finished_players[username] = [ids, distance]  # contain key=user, value=id and distance in list
+                    del players[username]  # delete player if player now in finished hashtable
+                    leader_board = ''  # string of leader_board
+                    if 3 <= len(finished_players) <= 5:  # condition for limit players count in leader_board (3-5)
+                        await message.answer(f'Таблица лидеров:')  # sent to user title of leader_board
+                        for idx, pl in enumerate(
+                                dict(sorted(finished_players.items(), key=lambda x: x[1], reverse=True)), start=1):
+                            # enumerate count and username to leader_board in nondecreasing order
+                            leader_board += f'{idx}. {pl} - {finished_players[pl][1]} метра\n'
+                        for ids in finished_players.values():
+                            await bot.send_message(chat_id=ids[0],
+                                                   text=leader_board)  # sent leader_board to every champion
+                        finished_players = {}  # clear for next race
 
-            await generate_leaderboard()
-        else:
-            players[username] = [start_latitude, start_longitude]
-            await message.answer(text='Гонка началась!', reply_markup=ReplyKeyboardRemove())
+                await generate_leaderboard()  # call function
+            else:
+                players[username] = [start_latitude, start_longitude]  # add player to all pool
+                await message.answer(text='Гонка началась!',
+                                     reply_markup=ReplyKeyboardRemove())  # close button until race not finished
 
-            async def set_timeouts():
-                secs = 2
-                timeout = secs / 2
-                while secs > 0:
-                    time.sleep(timeout)
-                    await message.answer(f'Осталось {secs} секунд до конца гонки!')
-                    secs -= int(timeout)
+                # function for limit timing of race
+                async def set_timeouts():
+                    secs = 2
+                    timeout = secs / 2
+                    while secs > 0:
+                        time.sleep(timeout)
+                        await message.answer(f'Осталось {secs} секунд до конца гонки!')
+                        secs -= int(timeout)
 
-            await set_timeouts()
-            race_done_mark = True
-            await message.answer(f'Гонка завершена')
-            await message.answer(
-                f'Нажми на кнопку "Узнать результат" в течении 30 секунд или дисквалификация...',
-                reply_markup=menu.mainMenuFinish)
+                await set_timeouts()
+                race_done_mark = True
+                await message.answer(f'Гонка завершена')  # bot sent 'Race over' to user
+                await message.answer(
+                    f'Нажми на кнопку "Узнать результат" в течении 30 секунд или дисквалификация...',
+                    reply_markup=menu.mainMenuFinish)  # Forces user to set second point of his location
 
-    await calculate_distance()
+        await calculate_distance()
 
 
 if __name__ == '__main__':
+    t = threading.Thread(target=main_func)  # create thread
+    t.start()  # start threading
     executor.start_polling(dp, skip_updates=True)
--- menu.py
+++ menu.py
@@ -5,9 +5,9 @@ from main import race_done_mark
 
 # --- Main Menu ---
 if race_done_mark is False:
-    btnStartGeo = KeyboardButton('🟢 Start 🟢', request_location=True)
-    mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnStartGeo)
-btnFinishGeo = KeyboardButton('🏁 Узнать результат 🏁', request_location=True)
-mainMenuFinish = ReplyKeyboardMarkup(resize_keyboard=True).add(btnFinishGeo)
+    btnStartGeo = KeyboardButton('🟢 Start 🟢', request_location=True)   # create start button
+    mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnStartGeo)   # add to markup
+btnFinishGeo = KeyboardButton('🏁 Узнать результат 🏁', request_location=True)   # create button for second point of location
+mainMenuFinish = ReplyKeyboardMarkup(resize_keyboard=True).add(btnFinishGeo)  # add finishbutton to changing markup
 
 
